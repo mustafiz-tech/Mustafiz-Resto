2dba0e568e200de28124c943219b6a79cf2f489a
mustafiz-tech <102187767+mustafiz-tech@users.noreply.github.com>
2022-08-08 11:39:19 +0530

    last change

--- js/myscript.js
+++ js/myscript.js
@@ -4,24 +4,24 @@
 $('.slider-main').slick({
     autoplay: true,
     autoplaySpeed: 2000,
-    dots:true,
-    arrows:true,
-    fade:true,
+    dots: true,
+    arrows: true,
+    fade: true,
     prevArrow: '<i class="fa  fa-arrow-left left-arrows">',
     nextArrow: '<i class="fa  fa-arrow-right right-arrows">',
 });
 
-$(function(){
-    $('.nav-button').click(function(){
+$(function () {
+    $('.nav-button').click(function () {
         $('ul').slideToggle(200)
     })
 })
 
-$(document).scroll(function(){
-    if ($(document).scrollTop() > 100){
-     $('nav').addClass('navScroll');
+$(document).scroll(function () {
+    if ($(document).scrollTop() > 100) {
+        $('nav').addClass('navScroll');
     }
-    else{
+    else {
         $('nav').removeClass('navScroll')
     }
 })
